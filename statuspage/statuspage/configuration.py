832b538815b72970aa181bb9abd91b8dab824b6f
NoaBenHamo <noab2001gmail.com>
2023-02-27 15:35:22 +0000

    changed Dockerfile (added user)

--- statuspage/statuspage/configuration.py
+++ statuspage/statuspage/configuration.py
@@ -1,4 +1,3 @@
-
 # Required Settings
 #
 
@@ -14,11 +13,7 @@ DATABASE = {
     'NAME': 'statuspage',         # Database name
     'USER': 'statuspage',               # PostgreSQL username
     'PASSWORD': 'Aa123456123456',           # PostgreSQL password
-<<<<<<< HEAD
-    'HOST': '10.0.18.29',      # Database server
-=======
-    'HOST': '10.0.18.29',      # Database server
->>>>>>> 7ae6cf6b55e6e1b2647b68774d548985ddd0049e
+    'HOST': '10.0.139.179',      # Database server
     'PORT': '5432',               # Database port (leave blank for default)
     'CONN_MAX_AGE': 300,      # Max database connection age
 }
@@ -27,11 +22,7 @@ DATABASE = {
 # for each. Full connection details are required.
 REDIS = {
     'tasks': {
-<<<<<<< HEAD
-        'HOST': '10.0.18.29',
-=======
-        'HOST': '10.0.18.29',
->>>>>>> 7ae6cf6b55e6e1b2647b68774d548985ddd0049e
+        'HOST': '10.0.139.179',
         'PORT': 6379,
         # Comment out `HOST` and `PORT` lines and uncomment the following if using Redis Sentinel
         # 'SENTINELS': [('mysentinel.redis.example.com', 6379)],
@@ -44,11 +35,7 @@ REDIS = {
         # 'INSECURE_SKIP_TLS_VERIFY': False,
     },
     'caching': {
-<<<<<<< HEAD
-        'HOST': '10.0.18.29',
-=======
-        'HOST': '10.0.18.29',
->>>>>>> 7ae6cf6b55e6e1b2647b68774d548985ddd0049e
+        'HOST': '10.0.139.179',
         'PORT': 6379,
         # Comment out `HOST` and `PORT` lines and uncomment the following if using Redis Sentinel
         # 'SENTINELS': [('mysentinel.redis.example.com', 6379)],
