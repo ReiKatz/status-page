48ea62f102639d12c7263438f8f289d3687855e4
Noa Ben Hamo <117725271+NoaBenHamo@users.noreply.github.com>
2023-03-02 10:38:15 +0200

    Create unit-tests.sh

---
+++ unit-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the Docker Compose file to use
+DOCKER_COMPOSE_FILE="./docker-compose.yml"
+
+# Install pytest-django
+pip install pytest-django
+
+# Start the Docker Compose services in detached mode
+docker-compose -f $DOCKER_COMPOSE_FILE up -d
+
+# Run the unit tests using the Docker Compose services
+docker-compose -f $DOCKER_COMPOSE_FILE run --rm app pytest ./unit-testing/test_script.py
+
+# Stop and remove the Docker Compose services
+docker-compose -f $DOCKER_COMPOSE_FILE down
