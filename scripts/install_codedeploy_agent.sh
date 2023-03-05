dbc12226670a01f6a0b6e22aba79f4345f5d0e36
Noa Ben Hamo <117725271+NoaBenHamo@users.noreply.github.com>
2023-03-05 20:32:04 +0200

    Create install_codedeploy_agent.sh

---
+++ scripts/install_codedeploy_agent.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/ubuntu
+sudo yum update -y
+sudo yum install -y ruby
+sudo yum install -y aws-cli
+wget https://aws-codedeploy-eu-west-1.s3.eu-west-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
