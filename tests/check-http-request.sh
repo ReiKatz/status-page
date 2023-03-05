4dd1c77c22967266149f9fb0d428cb940d95ff13
NoaBenHamo <noab2001gmail.com>
2023-03-05 09:09:40 +0000

    added bash script for testing and modified workflow

---
+++ tests/check-http-request.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PORT=8000
+ALB_DNS_NAME=finalproject-alb-2143497004.eu-west-1.elb.amazonaws.com
+
+# Make an HTTP request to the application through the ALB
+curl -I -s http://$ALB_DNS_NAME:$PORT/
+
+# Check the exit code of the curl command
+if [ $? -eq 0 ]; then
+  echo "HTTP request successful"
+else
+  echo "HTTP request failed"
+fi
+
