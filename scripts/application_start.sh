cf7c7bd73f83b11ed7e22abaa0979939a5aa6103
Noa Ben Hamo <117725271+NoaBenHamo@users.noreply.github.com>
2023-03-05 20:10:32 +0200

    Create application_start.sh

---
+++ scripts/application_start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+cd /home/ubuntu/status-page             # change to the status-page directory
+sudo docker-compose pull                # pull the latest images from the registry
+sudo docker-compose down                # stop and remove any existing containers
+sudo docker-compose up -d redis db      # start Redis and database containers
+sudo docker-compose up --build -d app   # start app container
