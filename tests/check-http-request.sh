31f3d82cb37a288385fbb359e5a11a3385ab54ba
NoaBenHamo <noab2001gmail.com>
2023-03-05 09:32:20 +0000

    modified test file

--- tests/check-http-request.sh
+++ tests/check-http-request.sh
@@ -7,7 +7,7 @@ ALB_DNS_NAME=finalproject-alb-2143497004.eu-west-1.elb.amazonaws.com
 curl -I -s http://$ALB_DNS_NAME:$PORT/
 
 # Check the exit code of the curl command
-if [ $? -eq 0 ]; then
+if [ $? -eq 200 ]; then
   echo "HTTP request successful"
 else
   echo "HTTP request failed"
