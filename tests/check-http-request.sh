7736d39567b2254f3aef54dc13b3befc5bc54255
NoaBenHamo <noab2001gmail.com>
2023-03-05 09:38:14 +0000

    modified test file

--- tests/check-http-request.sh
+++ tests/check-http-request.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 PORT=8000
-ALB_DNS_NAME=finalproject-alb-2143497004.eu-west-1.elb.amazonaws.com
+ALB_DNS_NAME=myapp-alb-1234567890.us-west-2.elb.amazonaws.com
 
 # Make an HTTP request to the application through the ALB
-curl -I -s http://$ALB_DNS_NAME:$PORT/
+RESPONSE=$(curl -I -s -w '%{http_code}' http://$ALB_DNS_NAME:$PORT/ -o /dev/null)
 
-# Check the exit code of the curl command
-if [ $? -eq 200 ]; then
+# Check the HTTP status code of the response
+if [ "$RESPONSE" -eq 200 ]; then
   echo "HTTP request successful"
 else
-  echo "HTTP request failed"
+  echo "HTTP request failed with status code $RESPONSE"
 fi
 
