da1c8340fc1964ec85d80c321908d6d9f1b9c6a2
Noa Ben Hamo <117725271+NoaBenHamo@users.noreply.github.com>
2023-03-05 16:09:21 +0200

    deleted py test script (won't be needed)

--- tests/test_script.py
+++
@@ -1,8 +0,0 @@
-# content of test_example.py
-from django.test import Client
-
-def test_example():
-    client = Client()
-    response = client.get('/')
-    assert response.status_code == 200
-    
