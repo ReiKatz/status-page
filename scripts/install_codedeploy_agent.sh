20588237fe904b406868b3aa126cb54cf4d80b62
Noa Ben Hamo <117725271+NoaBenHamo@users.noreply.github.com>
2023-03-06 22:14:04 +0200

    Delete install_codedeploy_agent.sh

--- scripts/install_codedeploy_agent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-cd /home/ubuntu
-sudo apt update -y
-sudo apt install -y ruby
-sudo apt install -y awscli
-wget https://aws-codedeploy-eu-west-1.s3.eu-west-1.amazonaws.com/latest/install
-sudo chmod +x ./install
-sudo ./install auto
