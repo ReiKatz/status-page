4862cc62b370b53a0e940d24e4a944e43f857350
ReiKatz <117725365+ReiKatz@users.noreply.github.com>
2023-03-06 16:42:14 +0200

    changing to postgres RDS

--- statuspage/statuspage/configuration.py
+++ statuspage/statuspage/configuration.py
@@ -18,7 +18,7 @@ DATABASE = {
     'NAME': 'statuspage',         # Database name
     'USER': 'statuspage',               # PostgreSQL username
     'PASSWORD': 'Aa123456123456',           # PostgreSQL password
-    'HOST': private_ip_address,      # Database server
+    'HOST': 'statuspage.cgw20yat8vpw.eu-west-1.rds.amazonaws.com',      # Database server
     'PORT': '5432',               # Database port (leave blank for default)
     'CONN_MAX_AGE': 300,      # Max database connection age
 }
