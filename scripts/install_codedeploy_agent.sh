dc1151b5593422d05145efee15201d304c52315a
Noa Ben Hamo <117725271+NoaBenHamo@users.noreply.github.com>
2023-03-06 11:20:50 +0200

    Update install_codedeploy_agent.sh

--- scripts/install_codedeploy_agent.sh
+++ scripts/install_codedeploy_agent.sh
@@ -3,7 +3,7 @@ set -e
 cd /home/ubuntu
 sudo yum update -y
 sudo yum install -y ruby
-sudo yum install -y aws-cli
+sudo yum install -y awscli
 wget https://aws-codedeploy-eu-west-1.s3.eu-west-1.amazonaws.com/latest/install
 sudo chmod +x ./install
 sudo ./install auto
