143ec5641ca47df7b250fc7d4d74b87a3daff127
ReiKatz <117725365+ReiKatz@users.noreply.github.com>
2023-03-07 10:33:47 +0200

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 cd /home/ubuntu/status-page             # change to the status-page directory
-
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 639771291841.dkr.ecr.eu-west-1.amazonaws.com #login to ECR
+$(aws ecr get-login --no-include-email --region eu-west-1)
+PASSWORD=$(aws ecr get-login-password --region eu-west-1)
+echo $PASSWORD | docker login --username AWS --password-stdin 639771291841.dkr.ecr.eu-west-1.amazonaws.com
+#aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 639771291841.dkr.ecr.eu-west-1.amazonaws.com #login to ECR
 docker pull 639771291841.dkr.ecr.eu-west-1.amazonaws.com/status-page:latest
 sudo docker run -d -p 8000:8000 --name appcontainer 639771291841.dkr.ecr.eu-west-1.amazonaws.com/status-page:latest
